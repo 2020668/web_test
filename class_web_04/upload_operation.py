06d7b50e41200daf560efdf82cf75b9804f95471
“2020668” <keen2020@outlook.com>
2019-07-28 18:56:34 +0800

    version 20190728

--- class_web_04/upload_operation.py
+++ class_web_04/upload_operation.py
@@ -12,12 +12,50 @@ E-mail:keen2020@outlook.com
 
 """
 
-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+import time
+import datetime
 import win32gui
 import win32con
 
 # edit – combobox – comboboxex32 - #32770
 # 前提 windows上传窗口 sleep2秒等待弹框的出现
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://www.ketangpai.com/")
+loc = By.XPATH, '//a[text()="登录"]'
+WebDriverWait(driver, 20).until(ec.visibility_of_element_located(loc))
+driver.find_element(*loc).click()
+
+loc = By.XPATH, '//input[@name="account"]'
+WebDriverWait(driver, 20).until(ec.visibility_of_element_located(loc))
+driver.find_element(*loc).send_keys("15071366971")
+
+driver.find_element_by_xpath('//input[@name="pass"]').send_keys("zuowei19881128")
+driver.find_element_by_xpath('//div[@class="padding-cont pt-login"]//a[text()="下次自动登录"]').click()
+driver.find_element_by_xpath('//div[@class="padding-cont pt-login"]//a[text()="登录"]').click()
+
+loc = By.XPATH, '//a[text()="Python全栈第18期"]'
+WebDriverWait(driver, 20).until(ec.visibility_of_element_located(loc))
+driver.find_element(*loc).click()
+
+time.sleep(3)
+
+loc = By.XPATH, '//a[contains(text(),"0726")]'
+WebDriverWait(driver, 20).until(ec.visibility_of_element_located(loc))
+driver.find_element(*loc).click()
+
+time.sleep(3)
+# loc = By.XPATH, '//div[text()="上传文件"]'
+loc = By.XPATH, '//label[@style="opacity: 0; width: 100%; height: 100%; ' \
+                'display: block; cursor: pointer; background: rgb(255, 255, 255);"]'
+# WebDriverWait(driver, 20).until(ec.visibility_of_element_located(loc))
+driver.find_element(*loc).click()
 
 
 def upload(filePath, browser_type="chrome"):
@@ -41,5 +79,9 @@ def upload(filePath, browser_type="chrome"):
     win32gui.SendMessage(dialog, win32con.WM_COMMAND, 1, button)      # 点击打开按钮
 
 
-upload("C:\\Users\keen\\Documents\\Software Testing\\Python自动化\\作业\\1830\\mouse_operation.py")
+time.sleep(3)
+upload("C:\\Users\keen\\Documents\\Software Testing\\Python自动化\\作业\\1831\\12306.py")
 # upload("C:\\aow_drv.log")
+
+time.sleep(3)
+driver.find_element_by_xpath('//a[text()="提交"]').click()
